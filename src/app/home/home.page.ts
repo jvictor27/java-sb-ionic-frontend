2b0903ad0906b247c11091d783e33944a515dc70
jvictor27 <jvictor451@gmail.com>
2019-10-24 18:31:39 -0300

    desabilitando menu no Login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 // import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, MenuController } from '@ionic/angular';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: 'home.page.html',
-	styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-	
-	constructor(public navCtrl: NavController) {}
-	
-	login() {
-		// this.router.navigate(['/categorias'])
-		this.navCtrl.navigateRoot('/categorias');
-	}
+
+    constructor(public navCtrl: NavController, public menu: MenuController) {}
+
+    login() {
+       this.navCtrl.navigateRoot('/categorias');
+    }
+    
+    ngOnInit() {
+        // this.menu.enable(false);
+    }
+    
+    ionViewWillEnter() {
+        this.menu.enable(false);
+    }
+    
+    ionViewDidLeave() {
+        this.menu.enable(true);
+    }
 }
