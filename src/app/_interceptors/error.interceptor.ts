d2e618f190698ddb2274e6571802d8b6f75c4d73
jvictor27 <jvictor451@gmail.com>
2019-11-28 21:25:03 -0300

    Tratamento padrão para outros erros com Alert

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -3,12 +3,15 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { StorageService } from '../_services/storage.service';
-
+import { AlertController } from '@ionic/angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){}
+    constructor(
+        public storage: StorageService,
+        public alertController: AlertController
+    ) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // console.log('Passou!!');
@@ -16,30 +19,49 @@ export class ErrorInterceptor implements HttpInterceptor {
         return next.handle(request)
             .pipe(
                 catchError(err => {
-                    let errorObj = err;
-                    if (errorObj.error) {
-                        errorObj = errorObj.error;
-                        if (!errorObj.status) {
-                            errorObj = JSON.parse(errorObj);
-                        }
-                    }
 
-                    switch (errorObj.status) {
+                    switch (err.status) {
+                        case 401:
+                            this.handled401();
+                            console.log('case 401:');
+                            break;
                         case 403:
-                            this.handle403();
+                            this.handled403();
                             break;
                         default:
+                            this.handledDefaultError(err);
                             break;
                     }
 
                     // console.log("Erro capturado pelo interceptor")
-                    // const error = err.error.message || err.statusText;
-                    return throwError(err.error);
+                    const error = err.error.message || err.statusText;
+                    return throwError(error);
+                    // return throwError(err.error);
                 })
             );
     }
 
-    handle403() {
+    async handledDefaultError(err: any) {
+        const alert = await this.alertController.create({
+            header: 'ERRO ' + err.status,
+            subHeader: err.error,
+            message: err.message,
+            buttons: ['OK']
+          });
+          await alert.present();
+    }
+
+    async handled401() {
+        const alert = await this.alertController.create({
+            header: 'ERRO 401',
+            subHeader: 'Falha de autenticação',
+            message: 'Email ou senha incorretos',
+            buttons: ['OK']
+          });
+          await alert.present();
+    }
+
+    handled403() {
         this.storage.setLocalUser(null);
     }
 }
