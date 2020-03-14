4d1ecb0d148732d88b33a12321983d6f57154c28
jvictor27 <jvictor451@gmail.com>
2020-03-14 09:52:04 -0300

    Refatorando cart.service.ts

--- src/app/_services/cart.service.ts
+++ src/app/_services/cart.service.ts
@@ -7,20 +7,22 @@ import { ProdutoDto } from '../_models/produto-dto';
 	providedIn: 'root'
 })
 export class CartService {
-
-	private quantity = {
+	
+	private action = {
+		ADD: 'add',
+		REMOVE: 'remove',
 		DECREASE: 'decrease',
 		INCREASE: 'increase'
 	};
-
+	
 	constructor(private storage: StorageService) { }
-
+	
 	createOrClearCartInLocalStorage(): Cart {
 		const cart: Cart = { items: [] };
 		this.storage.setCart(cart);
 		return cart;
 	}
-
+	
 	getCart(): Cart {
 		let cart: Cart = this.storage.getCart();
 		if (cart === null) {
@@ -28,64 +30,57 @@ export class CartService {
 		}
 		return cart;
 	}
-
+	
 	addProduto(produto: ProdutoDto): Cart {
-		return this.checkToRemoveOrAdd(produto);
+		return this.doActionInCart(produto, this.action.ADD);
 	}
-
+	
 	removeProduto(produto: ProdutoDto): Cart {
-		return this.checkToRemoveOrAdd(produto);
+		return this.doActionInCart(produto, this.action.REMOVE);
 	}
-
+	
 	increaseQuantity(produto: ProdutoDto): Cart {
-		return this.changeQuantity(produto, this.quantity.INCREASE);
+		return this.doActionInCart(produto, this.action.INCREASE);
 	}
-
+	
 	decreaseQuantity(produto: ProdutoDto): Cart {
-		return this.changeQuantity(produto, this.quantity.DECREASE);
+		return this.doActionInCart(produto, this.action.DECREASE);
 	}
-
+	
 	total(): number {
 		const cart = this.getCart();
 		let sum = 0;
-
+		
 		for (let i = 0; i < cart.items.length; i++) {
 			sum += cart.items[i].produto.preco * cart.items[i].quantidade;
 		}
-
+		
 		return sum;
 	}
-
-	private changeQuantity(produto: ProdutoDto, quantity: string): Cart {
+	
+	
+	private doActionInCart(produto: ProdutoDto, action: string ): Cart {
 		let cart = this.getCart();
 		const position = cart.items.findIndex(x => x.produto.id === produto.id);
-
-		if (position !== -1) {
-			if (quantity === this.quantity.DECREASE) {
-				cart.items[position].quantidade--;
-				if (cart.items[position].quantidade < 1) {
-					cart = this.removeProduto(produto);
-				}
-			} else if (quantity === this.quantity.INCREASE) {
-				cart.items[position].quantidade++;
-			}
-		}
-
-		this.storage.setCart(cart);
-		return cart;
-	}
-
-	private checkToRemoveOrAdd(produto: ProdutoDto): Cart {
-		const cart = this.getCart();
-
-
-		const position = cart.items.findIndex(x => x.produto.id === produto.id);
+		const prodExists = (position !== -1);
 		
-		if (position === -1) {
-			cart.items.push({ quantidade: 1, produto: produto });
-		} else {
-			cart.items.splice(position, 1);
+		switch (action) {
+			case this.action.ADD:
+			// if produto not exists in cart add; else incriese
+			(!prodExists) ? cart.items.push({quantidade: 1, produto: produto}) : this.increaseQuantity(produto);
+			break;
+			case this.action.REMOVE:
+			if (prodExists) {cart.items.splice(position, 1); }
+			break;
+			case this.action.INCREASE:
+			cart.items[position].quantidade++;
+			break;
+			case this.action.DECREASE:
+			cart.items[position].quantidade--;
+			if (cart.items[position].quantidade < 1) {cart = this.removeProduto(produto); }
+			break;
 		}
+		
 		this.storage.setCart(cart);
 		return cart;
 	}
