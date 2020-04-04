fe3e60e7422e43b5f773bfc8948629bc29394983
jvictor27 <jvictor451@gmail.com>
2020-04-04 13:32:19 -0300

    Fazendo upload da foto

--- src/app/_services/cliente.service.ts
+++ src/app/_services/cliente.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { StorageService } from './storage.service';
+import { ImageUtilService } from './image-util.service';
 import { ClienteDto } from '../_models/cliente-dto';
 import { PedidoDTO } from './../_models/pedido-dto';
 import { SignupPageModule } from './../signup/signup.module';
@@ -12,36 +13,51 @@ import { environment } from './../../environments/environment';
 	providedIn: 'root'
 })
 export class ClienteService {
-	
+
 	private pedidoParaFinalizar: PedidoDTO;
-	
-	constructor(public http: HttpClient, public storage: StorageService) { }
-	
+
+	constructor(
+		public http: HttpClient,
+		private imageUtilService: ImageUtilService,
+		public storage: StorageService) { }
+
 	findByEmail(email: string) {
 		return this.http.get(`${environment.baseUrl}clientes/email?value=${email}`);
 	}
-	
+
 	findById(id) {
 		return this.http.get(`${environment.baseUrl}clientes/${id}`);
 	}
-	
+
 	getImageFromBucket(id: string): Observable<any> {
 		const url = `${environment.bucketBaseUrl}cp${id}.jpg`;
 		return this.http.get(url, { responseType: 'blob' });
 	}
-	
+
 	inserir(cliente: ClienteDto) {
 		return this.http.post(`${environment.baseUrl}clientes`, cliente,
-		{
-			observe: 'response',
-			responseType: 'text'
-		});
+			{
+				observe: 'response',
+				responseType: 'text'
+			});
 	}
-	
+
+	uploadPicture(picture) {
+		const pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+		const formData: FormData = new FormData();
+		formData.set('file', pictureBlob, 'file.png');
+		return this.http.post(`${environment.baseUrl}clientes/picture`, formData,
+			{
+				observe: 'response',
+				responseType: 'text'
+			});
+
+	}
+
 	setPedidoParaFinalizar(pedido: PedidoDTO) {
 		this.pedidoParaFinalizar = pedido;
 	}
-	
+
 	getPedidoParaFinalizar(): PedidoDTO {
 		return this.pedidoParaFinalizar;
 	}
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -25,6 +25,10 @@ export class ProfilePage implements OnInit {
 		public clienteService: ClienteService) { }
 
 	ngOnInit() {
+		this.loadData();
+	}
+		
+	loadData() {
 		const localUser = this.storage.getLocalUser();
 		if (localUser && localUser.email) {
 			this.clienteService.findByEmail(localUser.email)
@@ -61,13 +65,24 @@ export class ProfilePage implements OnInit {
 		};
 	
 		this.camera.getPicture(options).then((imageData) => {
-		 	const mediaSource = new MediaSource();
-		 	const video = document.createElement('video');
 		 	this.picture = 'data:image/png;base64,' + imageData;
 		 	this.cameraOn = !this.cameraOn;
 		}, (err) => {
 			 // Handle error
 		});
+	}
+
+	sendPicture() {
+		this.clienteService.uploadPicture(this.picture)
+		.subscribe(response => {
+		  	this.cancel();
+		  	this.loadData();
+		},
+		error => {});
+	}
+	
+	cancel() {
+		this.picture = null;
 	}
 
 }
